458ff22982dff4057e458b256f43e15a1634c1e3
Amiel Perez <perezamiel@yahoo.com>
2018-09-23 22:17:36 +0800

    Implement single and double click

--- src/app.js
+++ src/app.js
@@ -1,9 +1,41 @@
-import { fromEvent } from 'rxjs';
+import { fromEvent, interval, merge } from 'rxjs';
+import { buffer, bufferTime , map, debounceTime, filter } from 'rxjs/operators'
 
-const button = document.getElementById('demo-btn');
-const output = document.getElementById('output');
+const clickbox = document.getElementById('clickbox');
+const message = document.getElementById('message');
 
-fromEvent(button, 'click')
-    .subscribe(() => {
-        output.textContent = `${(new Date()).toString()} (Using observables)`;
-    })
+const CLICK_INTERVAL_MS = 500;
+const IDLE_WINDOW_MS = 1200;
+
+const singleClickObservable$ = fromEvent(clickbox, 'click')
+    .pipe(
+        bufferTime(CLICK_INTERVAL_MS),
+        map((clicks) => clicks.length),
+        filter((length) => length === 1)
+    )
+    
+
+const multiClickObservable$ = fromEvent(clickbox, 'click')
+    .pipe(
+        bufferTime(CLICK_INTERVAL_MS),
+        map((clicks) => clicks.length),
+        filter((length) => length > 1)
+    )
+
+const idleObservable$ = merge(singleClickObservable$, multiClickObservable$)
+    .pipe(
+        debounceTime(IDLE_WINDOW_MS)
+    )
+
+singleClickObservable$.subscribe(() => {
+    message.textContent = 'Single clicked!';
+});
+
+multiClickObservable$.subscribe(() => {
+    message.textContent = 'Double+ clicked!';
+});
+
+idleObservable$.subscribe(() => {
+    message.textContent = '';
+})
+
