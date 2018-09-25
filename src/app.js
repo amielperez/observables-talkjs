fd3bb349741888eae7c55e5e3c609b519de7f894
N679247 <amiel.perez@jpmchase.com>
2018-09-25 10:47:54 +0800

    crude brush tool with dragging

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 import { fromEvent } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, repeat, skipUntil, takeUntil } from 'rxjs/operators';
 import { initCanvas, paintOn } from './utils';
 
 const canvas = document.getElementById('canvas');
 const ctx = initCanvas(canvas);
 
+const mouseUpObservable$ = fromEvent(document, 'mouseup');
+const mouseDownObservable$ = fromEvent(document, 'mousedown');
 const moveObservable$ = fromEvent(document, 'mousemove')
     .pipe(
-        map((pos) => ({ x: pos.clientX, y: pos.clientY }))
+        skipUntil(mouseDownObservable$),
+        map((pos) => ({ x: pos.clientX, y: pos.clientY })),
+        takeUntil(mouseUpObservable$),
+        repeat()
     );
 
 moveObservable$.subscribe((brushPos) => {
